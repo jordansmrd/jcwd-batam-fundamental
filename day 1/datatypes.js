fcbbbfaa84ae34c91dc3a152fe5bee9f21b17ffa
jordansmrd <jordansumardi@hotmail.com>
2022-11-08 12:12:24 +0700

    day 1

---
+++ day 1/datatypes.js
@@ -0,0 +1,39 @@
+// git 
+// clone => menarik source code dari online repo
+// commit => changes locally (menyimpan perubahan)
+// push  => push terhadap commit(mengirim hasil commit)
+// fetch => refresh dari local terhadap online ( mengetahui perubahan apa saja yg terjadi di online)
+// pull => menarik hasil perubahan yg terjadi di online ke local
+
+// repo edit 
+// repo source
+// fork => menyalin repo orang ke dalam repo kita 
+// repo orang tersebut kita edit lewat repo kita 
+// create pull request => repo edit melakukan request terhadap repo source 
+// agar perubahan yang terjadi direpo edit. masuk kedalam repo source
+
+// notif => ada pull request dari aaaaa 
+// repo source akan keganti sesuai dengan repo editnya
+
+console.log("Hello World!") // untuk menampilkan hello world
+console.log("hello")
+
+let message; //declare variable
+message = "Hello" // assign
+// var = redeclare dan reassign
+// let = reassign dan tidak dapat redeclare
+// const = tidak dapat reassign dan tidak dapat redeclare
+
+var a = "a"
+var a = "c"
+console.log(a)
+
+let b = "b"
+b = "c"
+console.log(b)
+
+const c = "c"
+console.log(c);
+
+const C = "z"
+console.log(C)
