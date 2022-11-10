b4762b17798c1a31d7ab4d7f3eb37839794a41a4
jordansmrd <jordansumardi@hotmail.com>
2022-11-10 09:21:32 +0700

    Create index.js

---
+++ day 3/index.js
@@ -0,0 +1,29 @@
+// simple array 
+let number = 1
+let string = " this is string "
+const arr = [10, 20, string]
+arr.push(number)
+
+let arrString = string.split("i")
+console.log(arrString);
+
+for (let i = 0; i < arrString.length ; i++) {
+    console.log(arrString[i]);
+    }
+    
+
+//index adalah alamat di dalam array. 
+console.log(arr);
+console.log(arr.length);
+
+
+
+for (let i = 0; i < arr.length ; i++) {
+console.log(arr[i]);
+}
+
+console.log(string.split("")
+);
+//array index starts from 0 . exmpl : arr[0]
+
+console.log(arr);
