47d5e1764334fe60d1afc77797f097260b7bba2b
jordansmrd <jordansumardi@hotmail.com>
2022-11-24 16:12:31 +0700

    exercise d 13

---
+++ day 13/exercise/class.js
@@ -0,0 +1,37 @@
+
+class Queue {
+   constructor() {
+    this.queue = []
+   }
+
+    addQueue(values){
+    let newData = {
+        "nama_makanan": values
+    }  
+    this.queue.push(newData);
+    } 
+
+    run(){
+    console.log(this.queue);
+        while(this.queue.length) {
+            let random =  Math.floor(Math.random() * 10) + 1 // random number dari 1 - 10
+
+            let promise = new Promise((resolve, reject) => { 
+                if(this.queue[0].nama_makanan) {
+                resolve("Queue nama makanan : " + this.queue[0].nama_makanan +" done in " + random + " minutes") 
+                }
+                else {
+                    reject("error nama makanan tidak ditemukan")
+                }
+            })
+            
+            setTimeout( () => promise.then((res) => console.log(res)) ,random)
+            this.queue.shift()
+        }
+    }
+}
+
+
+
+
+module.exports = Queue
--- day 13/index.js
+++ day 13/index.js
@@ -1,14 +1,14 @@
-//sync async
+// //sync async
 
-//sync 
-console.log(1);
-console.log(2);
-console.log(3);
+// //sync 
+// console.log(1);
+// console.log(2);
+// console.log(3);
 
-//async
-console.log(1);
-setTimeout(() => console.log(2), 3000);
-console.log(3);
+// //async
+// console.log(1);
+// setTimeout(() => console.log(2), 3000);
+// console.log(3);
 
 
 
@@ -23,9 +23,9 @@ const tryPromise = new Promise((resolve, reject) => {
 // .catch((err) => console.log(err))
 // .finally(()=> console.log("finally done"))
 
-const asyncAwait = async () => {
+const asyncAwait = () => {
     console.log("satu");
-  await tryPromise
+ tryPromise
 .then((res) => console.log(res))
 .catch((err) => console.log(err))
 .finally(()=> console.log("finally done"))
@@ -33,80 +33,80 @@ console.log("dua");
 
 }
 
-// asyncAwait()
+asyncAwait()
 
-const tryAndCatch = async () =>{
-    try {
-        let result = await tryPromise
-        console.log(result);
-    }
-    catch (error) {
-    console.log(error);
-    }
-}
+// const tryAndCatch = async () =>{
+//     try {
+//         let result = await tryPromise
+//         console.log(result);
+//     }
+//     catch (error) {
+//     console.log(error);
+//     }
+// }
 
-tryAndCatch()
-
-
-const tryThrow = () => {
-    try{
-    let isFalse = true;
-    if(isFalse) {
-        throw "there is an error"
-    }
-    console.log("Success");
-    // console.log("Success2");
-
-    }
-    catch (err) {
-    console.log(err);
-    }
-}
-
-tryThrow()
-
-// console.log("hello");
+// tryAndCatch()
 
 
-const tryThrow2 = () => {
+// const tryThrow = () => {
+//     try{
+//     let isFalse = true;
+//     if(isFalse) {
+//         throw "there is an error"
+//     }
+//     console.log("Success");
+//     // console.log("Success2");
+
+//     }
+//     catch (err) {
+//     console.log(err);
+//     }
+// }
+
+// tryThrow()
+
+// // console.log("hello");
+
+
+// const tryThrow2 = () => {
   
-    let isFalse = true;
-    if(isFalse) {
-        throw "there is an error"
-    }
-    console.log("Success");
+//     let isFalse = true;
+//     if(isFalse) {
+//         throw "there is an error"
+//     }
+//     console.log("Success");
    
-    console.log(err);
+//     console.log(err);
 
-}
+// }
 
-// tryThrow2()
-
-
-//json
-
-const json = '{ "student" :' +
- '[ { "name" : "jordan", "age" : 26} , ' +
-'{ "name" : "edbert", "age" : 27}] }'
-
-const jsonObj = JSON.parse(json)
-console.log(json);
-console.log(jsonObj.student[0].name);
+// // tryThrow2()
 
 
-const json2 = { student : [ { name : "jordan", age : 26} , { name : "edbert", age : 27}] }
+// //json
 
-console.log(json2.toString());
+// const json = '{ "student" :' +
+//  '[ { "name" : "jordan", "age" : 26} , ' +
+// '{ "name" : "edbert", "age" : 27}] }'
+
+// const jsonObj = JSON.parse(json)
+// console.log(json);
+// console.log(jsonObj.student[0].name);
+
+
+// const json2 = { student : [ { name : "jordan", age : 26} , { name : "edbert", age : 27}] }
+
+// console.log(json2.toString());
 
 
 
-console.log(JSON.stringify(json2));
+// console.log(JSON.stringify(json2));
 
 
-const { myFunction, name2 } = require('./data')
- console.log(name2);
- myFunction()
+// const { myFunction, name2 } = require('./data')
+//  console.log(name2);
+//  myFunction()
 
-const dataset = require('./data.json')
+// const dataset = require('./data.json')
 
-console.log(dataset);
+// console.log(dataset);
